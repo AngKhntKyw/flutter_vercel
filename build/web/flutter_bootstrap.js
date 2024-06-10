93d1dd65447fd25f04c8a177286a5d5d0703b1ab
Aung Khant Kyaw <aungkhantkyaw8918@gmail.com>
2024-06-11 02:35:52 +0630

    test

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1251371022"
+    serviceWorkerVersion: "103172547"
   }
 });
